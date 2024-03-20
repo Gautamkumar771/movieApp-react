f19a196836076bd65904363e53ff389b056728c3
Gautam kumar <gautamkumar844506@gmail.com>
2024-03-20 23:50:20 +0530

    adding navbar

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Navbar extends React.Component{
+    render(){
+        return(
+            <>
+        <div>
+           <div>Title</div>
+           <div>
+            <img alt="cart icon" src=""/>
+            <span>0</span>
+           </div>
+        </div>
+
+          
+          
+            </>
+        )
+    }
+}
+
+export default Navbar;
